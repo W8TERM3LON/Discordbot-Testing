f7d8ba2040e2075b65217e8d4ad09ff8837b263e
W8TERM3LON <davidjiang090@gmail.com>
2019-12-26 14:44:02 -0600

    2nd Commit

--- bot-start.py
+++ bot-start.py
@@ -17,6 +17,7 @@ async def on_ready():
     print('Online')
     return await bot.change_presence(activity=discord.Activity(name='AniList', type=0, state='Browsing.....', details='Testing', large_image_url='https://i.imgur.com/fqyhsZ5.png', small_image_url='https://i.imgur.com/fqyhsZ5.png', large_image_text='AniList', small_image_text='AniList'))
 
+#initial_extensions = ['cogs.findbots', 'cogs.global-links', 'cogs.global-pictures', 'cogs.profile-banner', 'cogs.profile-image', 'cogs.profile-links']
 #initial_extensions = ['cogs.Find', 'cogs.Global', 'cogs.Profile']
 initial_extensions = ['cogs.findbots']
 
--- cogs/findbots.py
+++ cogs/findbots.py
@@ -1,23 +1,35 @@
+#import
 import discord
-from discord.ext import commands
-import asyncio
-import datetime
+from discord.ext import commands, tasks
+import asyncio, aiohttp, re, time, datetime, json, os
+from itertools import cycle
+import aioschedule as schedule
+
+#Query Data
+accessToken = json.loads(open(os.path.join('', 'api.json')).read())
+
+no = 1
+
+payload = "{\"query\":\"query ($p: Int) {\\n  Page(page: $p) {\\n    users(sort: ID_DESC) {\\n      id\\n      about\\n      statistics{\\n        anime{\\n          count\\n        }\\n        manga{\\n          count\\n        }\\n      }\\n    }\\n  }\\n}\",\"variables\":{\"p\":" + str(no) + "}}"
+headers = {
+    'content-type': "application/json",
+        'authorization': "Bearer " + accessToken
+  }
 
 class FindBot(commands.Cog, name='FindBots'):
      
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command()
-    async def remove(self, ctx, *, number:int=None):
-        try:
-            if number is None:
-                await ctx.send('Number required....')
-            else:
-                deleted = await ctx.message.channel.purge(limit=number)
-                await ctx.send(f'Messages purged by {ctx.message.author.mention}: `{len(deleted)}`')
-        except:
-            await ctx.send("ERROR")
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Find Bots Ready')
+
+    @tasks.loop(seconds=5)
+    async def find(self):
+        pass
+
+        
     
 def setup(bot):
     bot.add_cog(FindBot(bot))
